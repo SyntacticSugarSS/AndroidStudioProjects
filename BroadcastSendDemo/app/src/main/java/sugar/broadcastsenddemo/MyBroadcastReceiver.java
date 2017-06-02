f146ace8c5de365e0e3c9babb185e6c8264b419b
SyntacticSugarSS <1393290187@qq.com>
2017-06-02 21:14:37 +0800

    标准广播

---
+++ BroadcastSendDemo/app/src/main/java/sugar/broadcastsenddemo/MyBroadcastReceiver.java
@@ -0,0 +1,16 @@
+package sugar.broadcastsenddemo;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.widget.Toast;
+
+public class MyBroadcastReceiver extends BroadcastReceiver {
+    public MyBroadcastReceiver() {
+    }
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        Toast.makeText(context, "received in MyBroadcastRecever", Toast.LENGTH_SHORT).show();;
+    }
+}
