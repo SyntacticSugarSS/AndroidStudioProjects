e5b9e552ad4e0a038734beb0a25914ad58b45da2
SyntacticSugarSS <1393290187@qq.com>
2017-06-03 15:59:03 +0800

    记住密码

--- BroadcastLogoutDemo/app/src/main/java/sugar/broadcastlogoutdemo/LoginActivity.java
+++ BroadcastLogoutDemo/app/src/main/java/sugar/broadcastlogoutdemo/LoginActivity.java
@@ -1,15 +1,22 @@
 package sugar.broadcastlogoutdemo;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.Toast;
 
 public class LoginActivity extends BestActivity {
 
+    private SharedPreferences pref;
+    private SharedPreferences.Editor editor;
+
+    private CheckBox rememberPass;
     private EditText accountEdit;
     private EditText passwordEdit;
     private Button login;
@@ -27,6 +34,17 @@ public class LoginActivity extends BestActivity {
                 String account = accountEdit.getText().toString();
                 String password = passwordEdit.getText().toString();
                 if (account.equals("admin")&&password.equals("123456")) {
+                    //登录成功.勾选了保存密码就保存下用户名
+                    editor = pref.edit();
+                    if (rememberPass.isChecked()) {
+                        editor.putBoolean("remember_password", true);
+                        editor.putString("account", account);
+                        editor.putString("password", password);
+                    }else {
+                        editor.clear();
+                    }
+                    editor.apply();
+
                     Intent intent = new Intent(LoginActivity.this, MainActivity.class);
                     startActivity(intent);
                     finish();
@@ -36,5 +54,16 @@ public class LoginActivity extends BestActivity {
                 }
             }
         });
+
+        pref = PreferenceManager.getDefaultSharedPreferences(this);
+        rememberPass = (CheckBox)findViewById(R.id.remember_pass);
+        boolean isRemember = pref.getBoolean("remember_password", false);
+        if (isRemember) {
+            String account = pref.getString("account", "");
+            String password = pref.getString("password", "");
+            accountEdit.setText(account);
+            passwordEdit.setText(password);
+            rememberPass.setChecked(true);
+        }
     }
 }
